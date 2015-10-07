3d90e4c8673673878f25427010595bdf7a57a10d
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-10-07 08:55:01 +0700

    busri

--- src/receiver.h
+++ src/receiver.h
@@ -1,12 +1,35 @@
+#ifndef receiver_h
+#define receiver_h
+
 #include "dcomm.h"
 #include "frame.h"
+#include "ackframe.h"
+
+#include <iostream>
+#include <string>
 
 class receiver{
 	public:
+	std::string popStringFromOneFrame();
+	receiver& operator>>(std::stream str);
+	bool listen(int port);
+	bool startReceiving();
+	bool stopReceiving();
+
 	private:
 	const int bufsize = 1024;
 	frame buffer[bufsize];
 	int windowposition;
 	const int windowsize=WINDOW_SIZE;
 	int buffertail; //menunjukkan posisi "ekor" buffer: yang belum dikonsumsi. apabila buffertail==windowposition maka buffer belum dapat digunakan
-}
+
+	//tambah variabel socket
+
+	//fungsi yang dipanggil thread dijadikan friend
+	friend void* /*mohon disesuaikan*/ receiving (void * callerobj);
+
+	//kontrol thread
+	int isReceiving;
+};
+
+#endif
---
+++ src/sender.h
@@ -0,0 +1,41 @@
+#ifndef sender_h
+#define sender_h
+
+#include "dcomm.h"
+#include "frame.h"
+#include "ackframe.h"
+
+#include <iostream>
+#include <string>
+
+class sender{
+	public:
+	sender& operator<<(std::string msg);
+	sender& operator<<(std::stream str);
+	bool connect(char* ip_str, int port);
+	bool startSending();
+	bool stopSending();
+
+	private:
+	const int bufsize = 1024;
+	frame buffer[bufsize];
+	int windowposition;
+	const int windowsize=WINDOW_SIZE;
+	int bufferhead; //menunjukkan posisi "kepala" buffer: Posisi frame terakhir yang di-buffer
+	bool sent [bufsize];
+	bool acked [bufsize];
+
+	//tambah variabel socket
+
+	//fungsi yang dipanggil thread dijadikan friend
+	friend void* /*mohon disesuaikan*/ sending (void * callerobj);
+	friend void* /*mohon disesuaikan*/ listenack (void * callerobj);
+
+	//kontrol thread
+	int isSending;
+
+	//fungsi helper
+	void geserJendela(int n);
+};
+
+#endif
