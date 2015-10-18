288299d4abdf322d7eac1d3e1d59e8c4f0d46fd1
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-10-18 07:36:42 +0700

    perbaikan sender sampe jalan

--- src/sender.cpp
+++ src/sender.cpp
@@ -11,34 +11,63 @@
 //kata nizam ada tiga thread: yang ngirim, yang nerima ack, dan yang consum
 
 void* sending (void * callerobj){
+	std::cout<<"sending thread online"<<std::endl;
+	static const float nonstricttimeoutseconds=0.3;
 	sender* senderobj = reinterpret_cast<sender*>(callerobj);
 	if ( senderobj != nullptr ) {
-		while ( senderobj->isSending ){
+		while ( senderobj->isSending || !senderobj->bufferIsEmpty()){
 			for(int i=0;i<senderobj->windowsize;i++){
 				int j = (senderobj->windowposition + i)%(senderobj->bufsize_sender);
+				if (senderobj->buffered[j])
 				if (!senderobj->sent[j]){
-					senderobj->sent[j] = false;
 					size_t sentBytes=sendto(senderobj->s, senderobj->buffer[j].getPointerToBytes(),senderobj->buffer[j].getBytesLength(),0,(struct sockaddr*) &senderobj->si_other ,sizeof(senderobj->si_other));
-				} else if (!senderobj->acked[j]){//tidak menerima ack atau menerima nak
+					senderobj->sent[j] = true;
+					senderobj->timesent[j]=clock();
+					std::cout<<"sent frame "<<senderobj->buffer[j].getFrameNumber()<<std::endl;
+				} else if (senderobj->sent[j] && !senderobj->acked[j]
+					&& (float(clock()-senderobj->timesent[j]))/CLOCKS_PER_SEC>nonstricttimeoutseconds){//tidak menerima ack atau menerima nak setelah timeout
 					size_t sentBytes=sendto(senderobj->s, senderobj->buffer[j].getPointerToBytes(),senderobj->buffer[j].getBytesLength(),0,(struct sockaddr*) &senderobj->si_other ,sizeof(senderobj->si_other));
+					senderobj->sent[j] = true;
+					senderobj->timesent[j]=clock();
+					std::cout<<"timeout, retry, sent frame "<<senderobj->buffer[j].getFrameNumber()<<std::endl;
+				} else if (j==senderobj->windowposition && senderobj->sent[j] && senderobj->acked[j]){
+					senderobj->geserJendela(1);
+					senderobj->sent[j]=false;
+					senderobj->acked[j]=false;
+					senderobj->buffered[j]=false;
 				}
 			}
-			usleep(100000);//delay
 		}
 	}
+	std::cout<<"sending thread offline"<<std::endl;
 }
 
 void* listenack (void * callerobj){
+	std::cout<<"listenack thread online"<<std::endl;
 	sender* senderobj = reinterpret_cast<sender*>(callerobj);
 	if ( senderobj != nullptr )
 	{
-	   while ( senderobj->isSending )
-	   {
-		//menerima ack
-//		int x=recvfrom(senderobj->s, [ga tau], [ga tau], 0,(struct sockaddr *) &caller->si_other, [ga tau]);
-		//if ack set ack jadi true
-	   }
+		socklen_t slen=sizeof(senderobj->si_other);
+		while ( senderobj->isSending || !senderobj->bufferIsEmpty())
+		{
+			//menerima ack
+			char bufchar[128];
+			int ackframelen=recvfrom(senderobj->s, bufchar, 128, 0,(struct sockaddr *) &senderobj->si_other, &slen);
+			ackframe AF(bufchar,ackframelen);
+			if (AF.isValid()){
+				int framenum=AF.getFrameNumber();
+				std::cout<<(AF.isACK()?"ACK ":"NAK ")<<framenum<<std::endl;
+				if (senderobj->sent[framenum])
+					if (AF.isACK())
+						senderobj->acked[framenum]=true;
+					else
+						senderobj->sent[framenum]=false;
+			}else{
+				std::cout<<"invalid ack received"<<std::endl;
+			}
+		}
 	}
+	std::cout<<"listenack thread offline"<<std::endl;
 }
 
 void* consume (void * callerobj){
@@ -78,12 +107,15 @@ void pthread_diep(const char *s)
 sender& sender::operator<<(std::string msg){
 /* Membuat frame dan memasukkannya ke buffer */
 /* Prekondisi: Buffer tidak penuh */
-	std::cout<<"zx"<<msg<<std::endl;
-	frame f(framenumber, msg);
+	std::cout<<"buffering "<<msg<<std::endl;
+	frame f(bufferhead, msg);
+	//wait
+	while(isBufferFull)usleep(100000);
 	//letakkan frame di buffer yang ke bufferHead
 	buffer[bufferhead] = f;
 	sent[bufferhead] = false;
-	framenumber++;
+	acked[bufferhead] = false;
+	buffered[bufferhead] = true;
 	geserBufferHead();	
 }
 
@@ -91,12 +123,21 @@ sender& sender::operator<<(std::istream& str){
 	
 	
 	char c;	
-	std::cout<<"xxx: "<<str<<std::endl;
+//	std::cout<<"xxx: "<<str<<std::endl;
 	//membaca seluruh str dan menyimpan ke string
-	while(str.get(c)){text += c;}
-	text+=Endfile;
 	startSending();
-	//stopSending(); //harusnya dipanggil waktu baca EOF
+	std::string msg="";
+	while(str.get(c)){
+		msg+=c;
+		if (msg.length()>=charperframe){
+			*this<<msg;
+			msg="";
+		}
+	}
+	msg+=Endfile;
+	*this<<msg;
+	
+	stopSending(); //harusnya dipanggil waktu baca EOF
 	return *this;
 }
 
@@ -121,17 +162,16 @@ bool sender::connect(const char* ip_str, int port){
 
 
 bool sender::startSending(){
-	//set semua sent[buffersize] menjadi true
-	//waktu frame dimasukkan ke buffer ke i, set sent[i] jadi false
-	//jadi yang dikirim pasti yang sent nya bernilai false
-	//buat menghindari ngirim frame kosong yang masuk bagian windows
-	for(int i=0;i<bufsize_sender;i++){
-
-	}
+	isSending=true;
+	if (pthread_create(&sending_tr,NULL,sending,this)) pthread_diep("error creating sending thread");
+	if (pthread_create(&listenack_tr,NULL,listenack,this)) pthread_diep("error creating listenack thread");
+	//if (pthread_create(&consume_tr,NULL,consume,this)) pthread_diep("error creating consume thread");
 }
 
 bool sender::stopSending(){
 	isSending = false;
+	pthread_join(listenack_tr,NULL);
+	pthread_join(sending_tr,NULL);
 }
 
 
--- src/sender.h
+++ src/sender.h
@@ -17,6 +17,7 @@
 #include <limits.h>
 #include <pthread.h>
 #include <signal.h>
+#include <time.h>
 
 class sender{
 	public:
@@ -27,14 +28,16 @@ class sender{
 	bool stopSending();
 
 	private:
-	static const int bufsize_sender = 10;
+	static const int bufsize_sender = bufsize;
 	
 	frame buffer[bufsize_sender];
 	int windowposition=0;
 	const int windowsize=WINDOW_SIZE;
 	int bufferhead=0; //menunjukkan posisi "kepala" buffer: Posisi frame terakhir yang di-buffer
-	bool sent [bufsize_sender];
-	bool acked [bufsize_sender];
+	bool sent [bufsize_sender] = { 0 };
+	bool acked [bufsize_sender] = { 0 };
+	bool buffered [bufsize_sender] = { 0 };
+	clock_t timesent [bufsize_sender];
 
 	bool isBufferFull=false;
 
@@ -43,7 +46,6 @@ class sender{
 
 	std::string text;
 
-	int framenumber = 0;
 	const int charperframe = 5;
 
 	//tambah variabel socket
@@ -53,11 +55,14 @@ class sender{
 	friend void* /*mohon disesuaikan*/ listenack (void * callerobj);
 	friend void* /*mohon disesuaikan*/ consume (void * callerobj);
 	//kontrol thread
+	pthread_t sending_tr, listenack_tr, consume_tr;
 	bool isSending=false;
 
 	//fungsi helper
 	void geserJendela(int n){windowposition=(windowposition+n)%bufsize_sender;isBufferFull=false;}
 	void geserBufferHead(){bufferhead=(bufferhead+1)%bufsize_sender;if(bufferhead==windowposition){isBufferFull=true;}}
+	bool bufferIsEmpty(){for (int i=0;i<bufsize_sender;i++) if (buffered[i])return false; return true;}
+
 };
 
 #endif
